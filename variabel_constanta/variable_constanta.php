14567e9871e91b4b76563d4ab4c797728c2d0b2c
Pasha Khatami Hasibuan <pashakhatamihsb@gmail.com>
2024-10-31 10:10:07 +0700

    feat : add variable.php

---
+++ variabel_constanta/variable_constanta.php
@@ -0,0 +1,16 @@
+/*
+* Variable
+- Nilai dapat berubah-ubah
+- Di PHP, tipe data sebuah variable juga bisa berubah-ubah
+- Penamaan variable di PHP diawali dengan dollar ($) diikuti nama variable
+- Aturan penamaan:
+- Hanya boleh diawali dengan huruf atau underscore(_)
+- Sensitif dengan huruf besar dan kecil
+- Tidak bisa diawali dengan angka
+- Hanya bisa breisi alpha-numeric dan underscore.
+*/
+
+<?php
+$nama = "Hello World";
+echo $hello;
+?>
