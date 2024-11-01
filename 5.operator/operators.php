b887d46284d1fbaca397ac592dc3afe77b7e69a6
Pasha Khatami Hasibuan <pashakhatamihsb@gmail.com>
2024-11-01 15:39:16 +0700

    feat: add folder operator

---
+++ 5.operator/operators.php
@@ -0,0 +1,53 @@
+<?php
+
+$uangAziz = 5000;
+$uangNajwa = 5000;
+$hargaSomay = 3000;
+
+// Penjumlahan
+echo $uangAziz + $hargaSomay;
+// Pengurangan
+echo $uangAziz - $hargaSomay;
+// Pembagian
+echo $uangAziz / $hargaSomay;
+// Perkalian
+echo $uangAziz * $hargaSomay;
+// Modulus
+echo $uangAziz % $hargaSomay;
+// Increment (++)
+$uangNajwa++;
+// Decrement (--)
+$uangNajwa--;
+// Pre Increment (++$uangNajwa);
+// Pre Decrement ($uangNajwa--);
+// Post Increment ($uangNajwa++);
+// Post Decrement ($uangNajwa--);
+
+// ---------------------------------
+
+// * Operator Assignment
+/*
+ * - Sama dengan (=) 
+ * - Penjumlahan sama dengan (+=)
+ * - Pengurangan sama dengan (-=)
+ * - Perkalian sama dengan (*=)
+ * - Pembagian sama dengan (/=)
+ * - Modulus sama dengan (%=)
+ */
+
+// * Operator Perbandingan
+/*
+ * - Sama dengan (==)
+ * - Tidak sama dengan (!=)
+ * - Lebih Besar (>)
+ * - Lebih Besar Sama Dengan (>=)
+ * - Lebih Kecil (<)
+ * - Lebih Kecil Sama Dengan (<=)
+ */
+
+// * Operator Logika
+/*
+ * - AND (&&)
+ * - OR (||)
+ * - NOT (!)
+ */
